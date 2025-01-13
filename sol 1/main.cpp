9f1543c0ae043b9177ac186e6537e72db3538404
NallidD <dorjahnd@gmail.com>
2025-01-13 12:36:29 -0600

    1. Two Sum Solution and Notes

---
+++ sol 1/main.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <unordered_map>
+
+class Solution {
+
+public:
+
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+
+        std::unordered_map<int, int> map;
+        std::vector<int> indices;
+
+        for (int i = 0; i < nums.size(); ++i) {
+        int num = nums[i];
+        int difference = target - num;
+
+            if (map.find(difference) != map.end()) {
+                indices.emplace_back(map[difference]);
+                indices.emplace_back(i);
+                return indices;
+            }
+
+            map[num] = i;
+        }
+
+        return indices;
+
+    }
+
+};
